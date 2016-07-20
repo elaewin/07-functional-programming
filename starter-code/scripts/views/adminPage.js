78c0ad923ca4a4ddfb2dea2e639731fa4eb79e5c
Erica Winberry <elaewin@gmail.com>
2016-07-19 17:50:46 -0700

    added IIFE to article.js, completed Article.numWordsAll and Article.allAuthors functions.

--- starter-code/scripts/views/adminPage.js
+++ starter-code/scripts/views/adminPage.js
@@ -5,6 +5,7 @@ articleView.initAdminPage = function() {
     $('.author-stats').append(template(stat));
   });
   $('#blog-stats .articles').text(Article.allArticles.length);
-  $('#blog-stats .words').text(Article.numWordsAll());
+  // $('#blog-stats .words').text(Article.numWordsAll());
 };
+
 Article.fetchAll(articleView.initAdminPage);
